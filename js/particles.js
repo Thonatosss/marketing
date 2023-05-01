3f9f88a0a75615ae103ff1c0bb21444f6d95a8b3
Mykola Romashenko <sven4369@gmail.com>
2023-05-01 16:29:22 +0300

    particles, logo

---
+++ js/particles.js
@@ -0,0 +1,10 @@
+window.onload = function () {
+    Particles.init({
+        selector: '.particles',
+        connectParticles: true,
+        speed: 0.2,
+        maxParticles: 50,
+        minDistance: 200,
+       
+    });
+};
