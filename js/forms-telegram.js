0c368dd9d66168408bb5337e1a730aaa49cff098
Mykola Romashenko <sven4369@gmail.com>
2023-05-01 22:52:52 +0300

    done with forms

--- js/forms-telegram.js
+++ js/forms-telegram.js
@@ -24,11 +24,102 @@ document.getElementById('first-form').addEventListener('submit', function (e) {
             if (!response.ok) {
                 throw new Error('Network response was not ok');
             }
-           
+            alert('Дякуємо за звернення!');
             this.firstFormPhone.value = '';
 
         })
         .catch(error => {
-            console.error('There was a problem sending the Telegram message:', error);
+            console.log(error);
+        });
+});
+
+
+const form = document.getElementById("msform");
+const submitButton = form.querySelector('input[type="submit"]');
+
+submitButton.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    const formData = new FormData(form);
+    const formDataObj = Object.fromEntries(formData);
+
+    let messageText = "Заявка з сайту!! \n\n";
+    for (const [key, value] of Object.entries(formDataObj)) {
+        messageText += `${key}: ${value} \n`;
+    }
+
+    fetch(URL, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Authorization": `Bearer ${TOKEN}`,
+        },
+        body: `chat_id=${CHAT_ID}&text=${messageText}`,
+    })
+        .then(response => {
+            
+        })
+        .catch(error => {
+            alert('Виникла помилка під час відправки даних. Спробуйте ще раз.');
+        });
+});
+
+
+document.getElementById('ouditForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+    let message = `Заявка з сайту!\n`;
+    message += `Ім'я:${this.oudit_user_name.value}\n`;
+    message += `Номер телефону:${this.oudit_user_phone.value}\n`
+    fetch(URL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            chat_id: CHAT_ID,
+            parse_mode: 'html',
+            text: `${message}`
+        })
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            
+            this.oudit_user_name.value = '';
+            this.ooudit_user_phone.value = '';
+
+        })
+        .catch(error => {
+            console.log(error);;
+        });
+});
+document.getElementById('modal_form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    let message = `Заявка з сайту!\n`;
+    message += `Ім'я:${this.modal_form_userName.value}\n`;
+    message += `Номер телефону:${this.modal_form_userPhone.value}\n`
+    fetch(URL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            chat_id: CHAT_ID,
+            parse_mode: 'html',
+            text: `${message}`
+        })
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+         
+            this.modal_form_userName.value = '';
+            this.modal_form_userPhone.value = '';
+
+        })
+        .catch(error => {
+            console.log(error);;
         });
 });
