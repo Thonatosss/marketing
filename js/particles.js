894f963946c7b278aecf1ec1b21794bc702250c2
Mykola Romashenko <sven4369@gmail.com>
2023-05-11 21:55:40 +0300

    particles

--- js/particles.js
+++ js/particles.js
@@ -1,35 +1,4 @@
-
-
-window.onload = function () {
-    Particles.init({
-        selector: '.particles',
-        color: '#ceb76e',
-        connectParticles: true,
-        responsive: [
-            {
-                breakpoint: 767,
-                options: {
-                    sizeVariations: 2,
-                    speed: 0.2,
-                    minDistance: 100,
-                }
-            },
-            {
-                breakpoint: 1999,
-                options: {
-                    sizeVariations: 3,
-                    speed: 0.3,
-                    minDistance: 150,
-                }
-            },
-            {
-                breakpoint: 3000,
-                options: {
-                    sizeVariations: 4,
-                    speed: 0.5,
-                    minDistance: 220,
-                }
-            }
-        ]
-    });
-};
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', 'particles.json', function () {
+    console.log('callback - particles.js config loaded');
+});
